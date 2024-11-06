393038b4e0f801d8d9dae7591086f1cc350b6ba2
Nikita <fomin.nv@phystech.edu>
2024-11-06 14:10:01 +0300

    pagination

--- app/Http/Controllers/Notebook/IndexController.php
+++ app/Http/Controllers/Notebook/IndexController.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Controllers\Notebook;
 
+use App\Http\Requests\Notebook\IndexRequest;
 use App\Models\Notebook;
 
 class IndexController extends BaseController
 {
-    public function __invoke() {
-        return Notebook::all();
+    public function __invoke(IndexRequest $request) {
+        $data = $request->validated();
+        $page = $per_page = 1;
+        if (isset($data['page'])) {
+            $page = $data['page'];
+            unset($data['page']);
+        }
+
+        if (isset($data['per_page'])) {
+            $per_page = $data['per_page'];
+            unset($data['per_page']);
+        }
+        return Notebook::paginate(perPage: $per_page, page: $page);
     }
 }
